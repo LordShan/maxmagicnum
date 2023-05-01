1cfde56b1f1ac4af183a42b45a5b4aeb7070424d
lordshan <mailshaytan@gmail.com>
2023-05-01 19:32:30 +0200

    AddClassMyNumber_woking

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
 import random
 from flask import Flask, jsonify, request, render_template
 
-
+class MyNumber:
+    
+    def __init__(self):
+        self.range = [1, 100]
+        self.attempts = []
+        self.number = random.randint(self.range[0], self.range[1])
+    
+    def reset(self):
+        self.range = [1, 100]
+        self.attempts.clear()
+        self.number = random.randint(self.range[0], self.range[1])
+    
+    def check_answer(self, answer):
+        if answer == "less":
+            self.range[1] = self.number - 1
+        elif answer == "more":
+            self.range[0] = self.number + 1
+        elif answer == "equals":
+            return f"Congratulations! You have guessed the number in {len(self.attempts)} attempts."
+        else:
+            return "Invalid answer. Please try again."
+        self.attempts.append(self.number)
+        self.number = random.randint(self.range[0], self.range[1])
+        return f"Number is {self.number}. Next guess?"
 
 
 num_mag:list = [] # список в котором хранится сгенерированное число
 attempt:list=[] # список в котором хранятся все (цифры) попытки. 
-list_genert=[1,100] # список в котором хранится range для random. 
+game = MyNumber()
 
 app = Flask(__name__) # присвоение переменой app объекта Flask(__name__) 
+
+
+
 
 @app.route('/') # декоратор для обработки Главной страницы
 def index():
@@ -34,34 +60,41 @@ def process_text():
 @app.route('/iPick', methods=['POST'])
 def iPick():
     
-    action = request.form.get('action')
+    ret_val=game.check_answer(request.form.get('action'))
+    return jsonify({'result': ret_val})
+    
+    
+    
+    
+    # myAttempt(request.form.get('action')).checkNum
+    # action = 
 
-    if action == 'more':
-        list_genert[0] = num_mag[0]
-        attempt.append(num_mag[0])
-    elif action == 'less':
-        list_genert[1] = num_mag[0]
-        attempt.append(num_mag[0])
-    elif action == 'equals':
-        number = f"I winner!!!{len(attempt)}-attempts<br>Reload page for try again"
-        list_genert=[1,100]
-        return jsonify({'result': number})
+    # if action == 'more':
+    #     list_genert[0] = num_mag[0]
+    #     attempt.append(num_mag[0])
+    # elif action == 'less':
+    #     list_genert[1] = num_mag[0]
+    #     attempt.append(num_mag[0])
+    # elif action == 'equals':
+    #     number = f"I winner!!!{len(attempt)}-attempts<br>Reload page for try again"
+    #     list_genert=[1,100]
+    #     return jsonify({'result': number})
         
-    while True:
-        if list_genert[0] - list_genert[1]==0: 
-            yr_num = list_genert[0]
-            list_genert=[1,100]
-            return jsonify({'result': f"Your {yr_num} {len(attempt)}-attempts<br>Reload page for try again"})
+    # while True:
+    #     if list_genert[0] - list_genert[1]==0: 
+    #         yr_num = list_genert[0]
+    #         list_genert=[1,100]
+    #         return jsonify({'result': f"Your {yr_num} {len(attempt)}-attempts<br>Reload page for try again"})
         
-        number = random.randint(list_genert[0], list_genert[1])
-        if number not in attempt:
-            number = random.randint(list_genert[0], list_genert[1])
-            num_mag.insert(0,number)
-        return jsonify({'result': f"My variant {number}"})
+    #     number = random.randint(list_genert[0], list_genert[1])
+    #     if number not in attempt:
+    #         number = random.randint(list_genert[0], list_genert[1])
+    #         num_mag.insert(0,number)
+    #     return jsonify({'result': f"My variant {number}"})
 
     
-    number = random.randint(list_genert[0], list_genert[1])
-    num_mag.insert(0,number)
+    # number = random.randint(list_genert[0], list_genert[1])
+    # num_mag.insert(0,number)
     
     
     
@@ -69,9 +102,9 @@ def iPick():
 
 
 def resetNumber():# сброс счетчиков и перегенерация нового числа
+    game.reset
     attempt.clear() # сброс счетчиков попыток
     num_mag.clear() # сброс сгененрированного числа
-    list_genert=[1,100]
     num_mag.append(random.randint(1, 100)) # перегенерация нового числа
 
 def youwish(num:int = 0,): # функция для обработки полученного числа, по умолчанию оно равна=0
