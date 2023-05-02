cf0d9f3f4bca3a33e6daf25d66694912d16bad91
lordshan <mailshaytan@gmail.com>
2023-05-02 11:26:43 +0200

    Realease

--- app.py
+++ app.py
@@ -2,38 +2,42 @@ import random
 from flask import Flask, jsonify, request, render_template
 
 class MyNumber:
-    
     def __init__(self):
-        self.range = [1, 100]
-        self.attempts = []
-        self.number = random.randint(self.range[0], self.range[1])
-    
-    def reset(self):
-        self.range = [1, 100]
-        self.attempts.clear()
-        self.number = random.randint(self.range[0], self.range[1])
-    
-    def check_answer(self, answer):
-        if answer == "less":
-            self.range[1] = self.number - 1
-        elif answer == "more":
-            self.range[0] = self.number + 1
-        elif answer == "equals":
-            return f"Congratulations! You have guessed the number in {len(self.attempts)} attempts."
-        else:
-            return "Invalid answer. Please try again."
-        self.attempts.append(self.number)
-        self.number = random.randint(self.range[0], self.range[1])
-        return f"Number is {self.number}. Next guess?"
+        self.generated_numbers = []
+        self.min_value = 1
+        self.max_value = 100
+        self.number = self.generate_number()
 
+    def reset(self):
+        self.min_value = 1
+        self.max_value = 100
+        self.generated_numbers = []
+        self.number = random.randint(self.min_value, self.max_value)
+    
+    def generate_number(self):
+        number = random.randint(self.min_value, self.max_value)
+        while number in self.generated_numbers:
+            number = random.randint(self.min_value, self.max_value)
+        self.generated_numbers.append(number)
+        return number
+
+    def check_guess(self, guess):
+        if guess == 'more':
+            self.min_value = self.number + 1
+            self.number = self.generate_number()
+            return f"Number is {self.number}. Next guess?"
+        elif guess == 'less':
+            self.max_value = self.number - 1
+            self.number = self.generate_number()
+            return f"Number is {self.number}. Next guess?"
+        elif guess == 'equals':
+            return f'I`m won! Number of attempts: {len(self.generated_numbers)}'
 
 num_mag:list = [] # список в котором хранится сгенерированное число
 attempt:list=[] # список в котором хранятся все (цифры) попытки. 
 game = MyNumber()
 
 app = Flask(__name__) # присвоение переменой app объекта Flask(__name__) 
-
-
 
 
 @app.route('/') # декоратор для обработки Главной страницы
@@ -60,49 +64,12 @@ def process_text():
 @app.route('/iPick', methods=['POST'])
 def iPick():
     
-    ret_val=game.check_answer(request.form.get('action'))
+    ret_val=game.check_guess(request.form.get('action'))
     return jsonify({'result': ret_val})
-    
-    
-    
-    
-    # myAttempt(request.form.get('action')).checkNum
-    # action = 
-
-    # if action == 'more':
-    #     list_genert[0] = num_mag[0]
-    #     attempt.append(num_mag[0])
-    # elif action == 'less':
-    #     list_genert[1] = num_mag[0]
-    #     attempt.append(num_mag[0])
-    # elif action == 'equals':
-    #     number = f"I winner!!!{len(attempt)}-attempts<br>Reload page for try again"
-    #     list_genert=[1,100]
-    #     return jsonify({'result': number})
-        
-    # while True:
-    #     if list_genert[0] - list_genert[1]==0: 
-    #         yr_num = list_genert[0]
-    #         list_genert=[1,100]
-    #         return jsonify({'result': f"Your {yr_num} {len(attempt)}-attempts<br>Reload page for try again"})
-        
-    #     number = random.randint(list_genert[0], list_genert[1])
-    #     if number not in attempt:
-    #         number = random.randint(list_genert[0], list_genert[1])
-    #         num_mag.insert(0,number)
-    #     return jsonify({'result': f"My variant {number}"})
-
-    
-    # number = random.randint(list_genert[0], list_genert[1])
-    # num_mag.insert(0,number)
-    
-    
-    
-
 
 
 def resetNumber():# сброс счетчиков и перегенерация нового числа
-    game.reset
+    game.reset()
     attempt.clear() # сброс счетчиков попыток
     num_mag.clear() # сброс сгененрированного числа
     num_mag.append(random.randint(1, 100)) # перегенерация нового числа
@@ -118,7 +85,7 @@ def youwish(num:int = 0,): # функция для обработки получ
             # логика проверки числа, в зависимости от результата формируется ответ для страницы
             if value == num:
                 txt_att=",".join(attempt)
-                return f"You win!!!  Your attempts {len(attempt)} \n list variants {txt_att}" 
+                return f"You win!!!  Your attempts {len(attempt)}<br> list variants {txt_att}" 
             elif value < num:
                 return f"Attempt {len(attempt)}. Number is less"
             elif value > num:
